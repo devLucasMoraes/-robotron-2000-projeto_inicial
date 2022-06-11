12966969edc08dfaaca2c835fca53e2511a64581
devLucasMoraes <devlucasmoraes@gmai.com>
2022-06-11 18:23:04 -0300

    operacao soma/subtrair

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-
-
-
-const robotron = document.querySelector('#robotron')
+/* const robotron = document.querySelector('#robotron')
 
 robotron.addEventListener('click', (evento) => {
     console.log(evento)
 })
+ */
+
+
+const braco = document.querySelector('#braco')
+
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        console.log(evento.target)
+        manipulaDados(evento.target.textContent)
+    })
+})
+
+
+function manipulaDados (operacao) {
+    if( operacao === "+") {
+        braco.value = parseInt(braco.value) + 1
+    } else {
+        braco.value = parseInt(braco.value) - 1
+    }
+}
